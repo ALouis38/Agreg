0f72f530cdcfcf009527e399fc805dcf18d33fca
Banwarth <pierre.banwarth@free.fr>
2015-04-30 15:27:07 +0200

    Ajout d'une classe de test, utilisation de twitter4j il reste a securiser la connexion ( meme si c'est un faux compte )

---
+++ src/main/java/org/news/agreg/Test.java
@@ -0,0 +1,16 @@
+package org.news.agreg;
+
+import java.net.URL;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Test {
+	public static void main(String[] args) {
+		DataFinder testImpl = new DataFinder();
+		HashMap<URL, String> res = (HashMap<URL, String>)testImpl.search("jeunes");
+		System.out.println("Resultat : ");
+		for(Entry<URL, String> s: res.entrySet()) {
+			System.out.println(s.getKey()+"\n"+s.getValue()+"\n");
+		}
+	}
+}
